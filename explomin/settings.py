cc3c93a1f25268403f1a5daf098acba71934e88b
mihairazvanu <mihairazvanuuu@gmail.com>
2022-07-15 01:56:17 +0300

    database

--- explomin/settings.py
+++ explomin/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'explomin1',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'explomin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'explomindb', 
+        'USER': 'postgres', 
+        'PASSWORD': 'speedcar11',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
