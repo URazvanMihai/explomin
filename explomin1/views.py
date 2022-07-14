3c7f9a4c9a523459d461738591a06195a301e018
mihairazvanu <mihairazvanuuu@gmail.com>
2022-07-15 02:47:31 +0300

    A

--- explomin1/views.py
+++ explomin1/views.py
@@ -1,9 +1,20 @@
 
+from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from explomin1.models import People
 
 # Create your views here.
 
 def index(request):
-    return render(request, "explomin1/index.html")
-
+  peoples = People.objects.all().values()
+  template = loader.get_template('explomin1/index.html')
+  context = {
+    'peoples': peoples,
+  }
+  return HttpResponse(template.render(context, request))
+  
+# def add(request):
+#   template = loader.get_template('add.html')
+#   return HttpResponse(template.render({}, request))
