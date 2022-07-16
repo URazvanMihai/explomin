5fcb0c9b9f3a81efa08f5027138a03333f8d91f6
mihairazvanu <mihairazvanuuu@gmail.com>
2022-07-16 22:23:06 +0300

    Update

--- explomin1/urls.py
+++ explomin1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.index, name="index")
-    
+    path("", views.index, name="index"),
+    path("/admin1", views.admin, name="admin")
 
 ]
--- explomin1/views.py
+++ explomin1/views.py
@@ -8,7 +8,7 @@ from explomin1.models import  People, Locations
 # Create your views here.
 
 def index(request):
-  
+  job = ['I','II','III']
   days = ['Luni', 'Marti', 'Miercuri', 'Joi',' Vineri']
   locations = Locations.objects.all().values()
   peoples = People.objects.all().values()
@@ -16,7 +16,24 @@ def index(request):
   context = {
     'peoples': peoples,
     'locations': locations,
-    'days': days
+    'days': days,
+    'job':job,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+
+def admin(request):
+  job = ['I','II','III']
+  days = ['Luni', 'Marti', 'Miercuri', 'Joi',' Vineri']
+  locations = Locations.objects.all().values()
+  peoples = People.objects.all().values()
+  template = loader.get_template('explomin1/admin.html')
+  context = {
+    'peoples': peoples,
+    'locations': locations,
+    'days': days,
+    'job':job,
   }
   return HttpResponse(template.render(context, request))
 
